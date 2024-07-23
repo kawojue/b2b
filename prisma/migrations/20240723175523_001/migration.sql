63942515451e6e5a98969a6609b542e61b448de5
Raheem Kawojue <kawojue08@gmail.com>
2024-07-23 18:58:59 +0100

    refactored

---
+++ prisma/migrations/20240723175523_001/migration.sql
@@ -0,0 +1,122 @@
+-- CreateEnum
+CREATE TYPE "CardStatus" AS ENUM ('active', 'frozen', 'pending', 'terminated');
+
+-- CreateEnum
+CREATE TYPE "CardType" AS ENUM ('virtual', 'giftcard');
+
+-- CreateEnum
+CREATE TYPE "CardBrand" AS ENUM ('visa', 'mastercard');
+
+-- CreateEnum
+CREATE TYPE "IDType" AS ENUM ('NIN', 'BVN', 'PASSPORT');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Business" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ownerId" UUID NOT NULL,
+
+    CONSTRAINT "Business_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Customer" (
+    "id" UUID NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
+    "idNumber" TEXT,
+    "idType" "IDType",
+    "userPhoto" TEXT,
+    "city" TEXT,
+    "state" TEXT,
+    "country" TEXT,
+    "zipCode" TEXT,
+    "line1" TEXT,
+    "houseNo" TEXT,
+    "bvn" TEXT,
+    "dob" TEXT,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "businessId" UUID NOT NULL,
+
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Card" (
+    "id" UUID NOT NULL,
+    "cardId" TEXT,
+    "cardName" TEXT,
+    "cardNumber" TEXT,
+    "last4" TEXT,
+    "cardType" "CardType",
+    "cardBrand" "CardBrand",
+    "expiry" TEXT,
+    "cvv" TEXT,
+    "valid" TEXT,
+    "reference" TEXT NOT NULL,
+    "billingAddress" JSONB,
+    "balance" DOUBLE PRECISION NOT NULL DEFAULT 0.00,
+    "status" "CardStatus" NOT NULL DEFAULT 'pending',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "customerId" UUID NOT NULL,
+
+    CONSTRAINT "Card_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Webhook" (
+    "id" SERIAL NOT NULL,
+    "url" TEXT NOT NULL,
+    "businessId" UUID NOT NULL,
+
+    CONSTRAINT "Webhook_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_name_idx" ON "User"("name");
+
+-- CreateIndex
+CREATE INDEX "Business_name_idx" ON "Business"("name");
+
+-- CreateIndex
+CREATE INDEX "Customer_firstname_lastname_email_idx" ON "Customer"("firstname", "lastname", "email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Card_cardId_key" ON "Card"("cardId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Card_reference_key" ON "Card"("reference");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Webhook_businessId_key" ON "Webhook"("businessId");
+
+-- AddForeignKey
+ALTER TABLE "Business" ADD CONSTRAINT "Business_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Customer" ADD CONSTRAINT "Customer_businessId_fkey" FOREIGN KEY ("businessId") REFERENCES "Business"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Card" ADD CONSTRAINT "Card_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Webhook" ADD CONSTRAINT "Webhook_businessId_fkey" FOREIGN KEY ("businessId") REFERENCES "Business"("id") ON DELETE CASCADE ON UPDATE CASCADE;
